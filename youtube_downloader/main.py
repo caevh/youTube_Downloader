e730328bdf57c8fd54ef53be776eb18d558eb7dc
caevh <harrycave1234@outlook.com>
2022-01-06 19:45:19 +0000

    Working code, prior to making chnages

--- youtube_downloader/main.py
+++ youtube_downloader/main.py
@@ -4,8 +4,9 @@ import subprocess
 import os
 import pdb
 
-yt = YouTube('https://www.youtube.com/watch?v=ffcitRgiNDs')
+yt = YouTube('https://www.youtube.com/watch?v=hmm6O25BOSg')
 video_title = yt.title + '.mp4'
+print(video_title)
 
 def video_audio_streams(yt):
     video_streams = yt.streams.filter(adaptive=True)
@@ -21,21 +22,27 @@ def video_audio_list(video_streams, audio_streams):
     return list_of_video_stream, list_of_audio_stream
 
 video_lst, audio_lst = video_audio_list(video_streams, audio_streams)
-def video_download(video_lst, audio_lst):
-    video = video_lst[0]
-    audio = audio_lst[0]
-    video.download('../output/video')
-    audio.download('../output/audio')
 
-video_download(video_lst, audio_lst)
+# print(video_lst)
+# def video_download(video_lst, audio_lst):
+#     video = video_lst[0]
+#     audio = video_lst[-1]
+#     #video.download('../output/video')
+#     audio.download('../output/audio')
 
-def file_renaming():
-    global video_title
-    truncated_name = video_title[:4] + ".mp4"
-    os.rename(f'../output/audio/{video_title}', f'../output/audio/' + truncated_name)
-    os.rename(f'../output/video/{video_title}', f'../output/video/' + truncated_name)
-    merge_audio_and_video_streams(truncated_name)
+#video_download(video_lst, audio_lst)
+# #pdb.set_trace()
+# def file_renaming():
+#     global video_title
+#     video_title = video_title.translate({ord(i): None for i in '|'})
+#     truncated_name = video_title[:4].replace(' ', '') + ".mp4"
+#     os.rename(f'../output/audio/{video_title}', f'../output/audio/' + truncated_name)
+#     os.rename(f'../output/video/{video_title}', f'../output/video/' + truncated_name)
+#     return truncated_name
 
-def merge_audio_and_video_streams(truncated_name):    
-    subprocess.call(['/home/harry/Projects/youTube_downloader/youtube_downloader/video_audio_merge.sh', truncated_name])
-file_renaming()
+# truncated_name = file_renaming()
+
+# def merge_audio_and_video_streams(truncated_name):    
+#     subprocess.call(['/home/harry/Projects/youTube_downloader/youtube_downloader/video_audio_merge.sh', truncated_name])
+
+# merge_audio_and_video_streams(truncated_name)
